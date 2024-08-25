4e0a703714f0ac2597da06c9a1cdc8f3fb092685
Xamir17guerra <119978304+Xamir17guerra@users.noreply.github.com>
2024-08-25 15:47:13 -0500

    Add files via upload
    Initial commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por contactarnos, te responderemos pronto.');
+    this.reset();
+});
